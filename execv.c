0c94195b8130ff1537cec21751cfda3f1f68480f
ohendarko <ohendarko@gmail.com>
2023-09-19 01:14:04 +0000

    Encapsulations and split out functions to make it betty compliant

---
+++ execv.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "main.h"
+
+/*Change directory implementation*/
+void exec_comm(char *token)
+{
+	char *args[20];/*Adjust the size as needed*/
+ 
+	args[0] = token;
+	int argCount = 1;
+	while ((token = _strtok(NULL, " ")) != NULL)
+	{
+		args[argCount++] = token;
+	}
+	args[argCount] = NULL;/*Null-terminate the argument vector*/
+	
+	pid_t pid = fork();/*Fork and execute the command*/
+
+	if (pid < 0)
+	{
+		perror("Fork failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)/*Child process*/
+	{
+		if (execvp(args[0], args) < 0)
+		{
+			perror("Execution failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else/*Parent process*/
+	{
+		int status;
+
+		waitpid(pid, &status, 0);
+	}
+}
