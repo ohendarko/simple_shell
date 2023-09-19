083636c2a36e46613df7272bb0aa3a87f7253f62
ohendarko <ohendarko@gmail.com>
2023-09-19 20:48:27 +0000

    betty compliant

--- shell_trialB.c
+++ shell_trialB.c
@@ -22,34 +22,28 @@ int main(void)
 		{
 			perror("Error reading input");
 			exit(EXIT_FAILURE);
-		}/*Remove the newline character from the input*/
-		length = strlen(input);
-		 if (length > 0 && input[length - 1] == '\n')
-		 {
-			 input[length - 1] = '\0';
-		 }/*Split the input into command and arguments*/
+		}
 
-		token = strtok(input, " ");
+		length = strlen(input);/*Remove the newline character from the input*/
+		if (length > 0 && input[length - 1] == '\n')
+		{
+			input[length - 1] = '\0';
+		}
+		token = strtok(input, " ");/*Split the input into command and arguments*/
 
 		if (token == NULL)
-		{
 			continue;/*Empty input, prompt again*/
-		}
 		if (strcmp(token, "exit") == 0)/*Check for exit command*/
 			exit(0);
 		if (strcmp(token, "cd") == 0)/*Change directory implementation*/
 		{
 			token = strtok(NULL, " ");
 			if (token == NULL)
-			{
 				chdir(getenv("HOME"));
-			}
 			else
 			{
 				if (chdir(token) == -1)
-				{
 					perror("-bash : cd");
-				}
 			}
 			continue;
 		}
