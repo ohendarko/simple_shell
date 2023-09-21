df3cb20197ff8d780696a404e8de5d3ef6da8947
ohendarko <ohendarko@gmail.com>
2023-09-21 17:20:52 +0000

    did some stuff

--- _atoi.c
+++ _atoi.c
@@ -1,62 +1,58 @@
 #include "shell.h"
-
 /**
- * check_deli - checks if character is a delimiter
- * @c: the char to check
- * @delim: the delimiter string
- * Return: 1 if true, 0 if false
- */
-int check_deli(char c, char *delim)
+* strt_nt - converts a string to an integer
+* @s: the string to be converted
+* Return: 0 if no numbers in string, converted number otherwise
+*/
+int strt_nt(char *s)
 {
-	while (*delim)
-		if (*delim++ == c)
+	int i, gn = 1, fl = 0, utt;
+	unsigned int res = 0;
+
+	for (i = 0; s[i] != '\0' && fl != 2; i++)
+	{
+		if (s[i] == '-')
+			gn *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			fl = 1;
+			res *= 10;
+			res += (s[i] - '0');
+		}
+		else if (fl == 1)
+			fl = 2;
+	}
+
+	if (gn == -1)
+		utt = -res;
+	else
+		utt = res;
+
+	return (utt);
+}
+/**
+* check_deli - checks char s is delimiter
+* @s: char to check
+* @Delim: the delimiter string
+* Return: 1 true, 0 false
+*/
+int check_deli(char s, char *Delim)
+{
+	while (*Delim)
+		if (*Delim++ == s)
 			return (1);
 	return (0);
 }
-
 /**
- * is_alpha - checks for alphabetic character
- * @c: The character to check
- * Return: 1 if c is alphabetic, 0 otherwise
- */
-int is_alpha(int c)
+* is_alpha - checks for alphabetic character
+* @c: The character to check
+* Return: 1 if c is alphabetic, 0 otherwise
+*/
+int is_alpha(int s)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((s >= 'a' && s <= 'z') || (s >= 'A' && s <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
-
-/**
- * strt_nt - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
- */
-int strt_nt(char *s)
-{
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
-
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
-
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
-}
-
