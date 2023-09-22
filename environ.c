64c897cecc40c52b9d0254da667804555ee0176c
Drreey <erzuahirene@gmail.com>
2023-09-21 18:10:50 -0700

    environ-new

---
+++ environ.c
@@ -0,0 +1,92 @@
+#include "shell.h"
+
+/**
+ * print_curenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int print_curenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * varof_envget - gets the value of an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env variable name
+ * Return: the value
+ */
+char *varof_envget(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * initnew_envvar - Initialize a new environment variable,
+ *                  or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * Return: Always 0
+ */
+int initnew_envvar(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguments\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * rmenv_var - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * Return: Always 0
+ */
+int rmenv_var(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguments.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * opu_envil - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *            constant function prototype.
+ * Return: Always 0
+ */
+int opu_envil(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
+
